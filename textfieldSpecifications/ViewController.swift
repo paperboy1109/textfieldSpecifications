73a0db62f8ee020448fb5953800bfe68f7c0dbe2
Daniel Janiak <danieljjaniak@gmail.com>
2016-05-21 15:09:16 -0600

    Initial Commit

---
+++ textfieldSpecifications/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  textfieldSpecifications
+//
+//  Created by Daniel J Janiak on 5/21/16.
+//  Copyright © 2016 Daniel J Janiak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
